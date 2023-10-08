1f9229a2c71920f40dceed7ca746f6dddd418d52
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-10-08 20:10:45 -0300

    Update main.mjs

--- code/main.mjs
+++ code/main.mjs
@@ -4,6 +4,7 @@ import Game from './scenes/game.mjs'
 import Menu from './scenes/menu.mjs'
 import Credits from './scenes/credits.mjs'
 import Dev from './scenes/devScreen.mjs'
+import Shop from './scenes/shop.mjs'
 // initialize context
 kaboom({
 	width: window.innerWidth,
@@ -11,7 +12,7 @@ kaboom({
 	backgroundAudio: true,
 	background: [0,20,102],
 	loadingScreen: false,
-	// canvas: document.getElementById("gamecanvas")
+	canvas: document.getElementById("gamecanvas")
 })
 
 if(!localStorage.getItem("score")) {
@@ -115,6 +116,13 @@ scene("credits", () => {
 	menumusic.volume = 0
 	gamemusic.volume = 0
 	Credits();
+})
+scene("shop", () => {
+	creditsmusic.play()
+	creditsmusic.volume = 1
+	menumusic.volume = 0
+	gamemusic.volume = 0
+	Shop();
 })
 scene("loading", () => {
 	var progress = 0
