ab35e4e6112af5ef00f58acd1684e059f9bd011e
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-10-10 18:20:32 -0300

    Update shop.mjs

--- code/scenes/shop.mjs
+++ code/scenes/shop.mjs
@@ -1,7 +1,32 @@
 import kaboom from 'kaboom'
-
+const items = [
+  {
+    sprite: "floosh",
+    name: "Floosh",
+    price: 0
+  },
+  {
+    sprite: "nerd",
+    name: "Nerd",
+    price: 100
+  }
+]
 export default function Shop() {
+  items.map((item, index) => {
+    add([
+      sprite(item.sprite),
+      pos(height()/2, 2*index),
+      area(),
+      item.name
+    ])
+  })
   add([
-    text("Em breve...")
+    text("Voltar"),
+    pos(10, 10),
+    area(),
+    "backbtn"
   ])
+  onClick("backbtn", () => {
+    go("menu")
+  })
 }
