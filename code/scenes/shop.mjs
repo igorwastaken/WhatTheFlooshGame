5550960f19251fb445d813325209df0bbb5ec90a
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-10-10 18:55:36 -0300

    Update shop.mjs

--- code/scenes/shop.mjs
+++ code/scenes/shop.mjs
@@ -15,7 +15,7 @@ export default function Shop() {
   items.map((item, index) => {
     const i = add([
       sprite(item.sprite),
-      pos(width()/2.3, 20*(index)),
+      pos(width()/2.3, 40*(index)),
       area(),
       item.name
     ])
