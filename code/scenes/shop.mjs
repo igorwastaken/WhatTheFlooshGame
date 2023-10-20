10ad3d983ed434a58092a22a9eb604818671a1b4
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-10-19 21:57:27 -0300

    Better shop.mjs

--- code/scenes/shop.mjs
+++ code/scenes/shop.mjs
@@ -1,43 +1,42 @@
 import kaboom from 'kaboom'
-const items = [
-  {
-    sprite: "bean",
-    name: "Floosh",
-    price: 0
-  },
-  {
-    sprite: "nerd",
-    name: "Nerd",
-    price: 100
-  }
+const items = [{
+        sprite: "bean",
+        name: "Floosh",
+        price: 0
+    },
+    {
+        sprite: "nerd",
+        name: "Nerd",
+        price: 100
+    }
 ]
 export default function Shop() {
-  items.map((item, index) => {
-    const i = add([
-      sprite(item.sprite),
-      pos(width()/2.3, 55*(index)),
-      area(),
-      opacity(localStorage.getItem("score")>=item.price?1:0.5),
-      item.name
-    ])
-    onClick(item.name, () => {
-      if(localStorage.getItem("score")>=item.price) {
-        localStorage.setItem("skin", item.sprite)
-        go("menu")
-      } else {
-        alert("Desculpe, mas você não tem pontos suficientes para comprar esse item.")
-      }
+    items.map((item, index) => {
+        const i = add([
+            sprite(item.sprite),
+            pos(width() / 2.3, 55 * (index)),
+            area(),
+            opacity(localStorage.getItem("score") >= item.price ? 1 : 0.5),
+            item.name
+        ])
+        onClick(item.name, () => {
+            if (localStorage.getItem("score") >= item.price) {
+                localStorage.setItem("skin", item.sprite)
+                go("menu")
+            } else {
+                alert("Desculpe, mas você não tem pontos suficientes para comprar esse item.")
+            }
+        })
     })
-  })
-  add([
-    text("Voltar", {
-      size: 14
-  }),
-    pos(10, 10),
-    area(),
-    "backbtn"
-  ])
-  onClick("backbtn", () => {
-    go("menu")
-  })
+    add([
+        text("Voltar", {
+            size: 14
+        }),
+        pos(10, 10),
+        area(),
+        "backbtn"
+    ])
+    onClick("backbtn", () => {
+        go("menu")
+    })
 }
