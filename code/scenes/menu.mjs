946c200e7b3ad30ddb88ceeb37c878e2f4b4d6ae
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-10-19 21:58:59 -0300

    Better menu.mjs

--- code/scenes/menu.mjs
+++ code/scenes/menu.mjs
@@ -1,141 +1,140 @@
-// import 'kaboom/global'
 import kaboom from 'kaboom'
 
 export default function MainMenu() {
-	const maxScore = localStorage.getItem("score");
-	var afkTimeout = 0;
-	add([
+    const maxScore = localStorage.getItem("score");
+    var afkTimeout = 0;
+    add([
         text(`What The Floosh Game`, {
             size: 28,
             width: width(),
-            // font: "breakout"
         }),
         pos(10, 10),
-		z(3)
+        z(3)
     ]);
-	add([
-		text("Maior pontuação: " + maxScore, {
-			size: 18
-		}),
-		pos(10, 60),
-		"score",
-		z(3)
-	])
-	add([
-		text(localStorage.getItem("coins"), {size: 18}),
-		pos(40, 93),
-		"coins",
-		z(3)
-	])
-	add([
-               sprite("coin"),
-               pos(10,90),
-               "coins",
-               z(3),
-               scale(0.1)
-        ])
-	add([
-		pos(center()),
-		sprite(localStorage.getItem("skin")),
-		area(),
-		body(),
-		"btn",
-		z(1),
-	])
-	add([
+    add([
+        text("Maior pontuação: " + maxScore, {
+            size: 18
+        }),
+        pos(10, 60),
+        "score",
+        z(3)
+    ])
+    add([
+        text(localStorage.getItem("coins"), {
+            size: 18
+        }),
+        pos(40, 93),
+        "coins",
+        z(3)
+    ])
+    add([
+        sprite("coin"),
+        pos(10, 90),
+        "coins",
+        z(3),
+        scale(0.1)
+    ])
+    add([
+        pos(center()),
+        sprite(localStorage.getItem("skin")),
+        area(),
+        body(),
+        "btn",
+        z(1),
+    ])
+    add([
         text(`Jogar`, {
             size: 16,
             width: width(),
-	    align: "center"
-            // font: "breakout"
+            align: "center"
         }),
-        pos(0, height()/2.9),
-		area(),
-		"btn",
-		z(3),
-		// outline(10, BLACK)
+        pos(0, height() / 2.9),
+        area(),
+        "btn",
+        z(3),
     ]);
-	add([
-		text(`Créditos`, {
-			size: 16,
-			width: width(),
-			align: "center"
-		}),
-		pos(0, height()/2.5),
-		area(),
-		"credits",
-		z(3)
-	])
-	add([
-		text("Loja", {
-			size: 16,
-			width: width(),
-			align:"center"
-		}),
-		pos(0,height()/2.2),
-		area(),
-		"shop",
-		z(3)
-	])
+    add([
+        text(`Créditos`, {
+            size: 16,
+            width: width(),
+            align: "center"
+        }),
+        pos(0, height() / 2.5),
+        area(),
+        "credits",
+        z(3)
+    ])
+    add([
+        text("Loja", {
+            size: 16,
+            width: width(),
+            align: "center"
+        }),
+        pos(0, height() / 2.2),
+        area(),
+        "shop",
+        z(3)
+    ])
     var clouds = 0
-	for(clouds=0; clouds < 50; clouds++) {
-		const clouds = add([
-			sprite("star"),
-			area(),
-			pos(rand(width()), rand(height())),
-			scale(rand(0.1, 0.3)),
-			rotate(rand(0,360)),
-			"stars"
-		])
-		/*clouds.onCollide("score", () => destroy(clouds))*/
-	}
-	function spawnClouds() {
-	    const recta = add([
-            pos(width(), rand(height())),
-		    sprite("cloud"),
-            //outline(2),
-			scale(0.3),
+    for (clouds = 0; clouds < 50; clouds++) {
+        const clouds = add([
+            sprite("star"),
             area(),
-			offscreen({destroy:true}),
-			"clouds",
-			z(rand(0, 2))
+            pos(rand(width()), rand(height())),
+            scale(rand(0.1, 0.3)),
+            rotate(rand(0, 360)),
+            "stars"
         ])
-		onUpdate(() => {recta.move(rand(-150,-100),0)})
-		//wait(rand(0.3, 0.7), spawnRect);
-		wait(0.3, spawnClouds);
-		recta.onUpdate(() => {
-		if (recta.pos.x > width()) {
-			destroy(recta)
-			// addKaboom(recta.pos)
-		}
-	})
-	}
-	add([
-		text("2023 © Igor Figueiredo", {
-			size: 15,
-			width: width()
-		}),
-		pos(10, height()-30)
-	])
-	spawnClouds()
-	onCollide("btn", (e) => {
-		destroy(e)
-	})
-	onCollide("stars", "stars", (s) => {
-		destroy(s)
-	})
-	onCollide("credits", (e) => {
-		destroy(e)
-	})
-	onClick("btn", () => {
-		go("game")
-	})
-	onClick("credits", () => {
-		go("credits")
-	})
-	onClick("shop", () => {
-		go("shop")
-	})
+    }
+
+    function spawnClouds() {
+        const recta = add([
+            pos(width(), rand(height())),
+            sprite("cloud"),
+            scale(0.3),
+            area(),
+            offscreen({
+                destroy: true
+            }),
+            "clouds",
+            z(rand(0, 2))
+        ])
+        onUpdate(() => {
+            recta.move(rand(-150, -100), 0)
+        })
+	wait(0.3, spawnClouds);
+        recta.onUpdate(() => {
+            if (recta.pos.x > width()) {
+                destroy(recta)
+            }
+        })
+    }
+    add([
+        text("2023 © Igor Figueiredo", {
+            size: 15,
+            width: width()
+        }),
+        pos(10, height() - 30)
+    ])
+    spawnClouds()
+    onCollide("btn", (e) => {
+        destroy(e)
+    })
+    onCollide("stars", "stars", (s) => {
+        destroy(s)
+    })
+    onCollide("credits", (e) => {
+        destroy(e)
+    })
+    onClick("btn", () => {
+        go("game")
+    })
+    onClick("credits", () => {
+        go("credits")
+    })
+    onClick("shop", () => {
+        go("shop")
+    })
     onMouseMove(() => {
         afkTimeout = 0;
     })
@@ -147,8 +146,8 @@ export default function MainMenu() {
     })
     onUpdate(() => {
         console.log(afkTimeout);
-        if(afkTimeout === 60) {
+        if (afkTimeout === 60) {
             go("afk")
         }
-	})
+    })
 }
