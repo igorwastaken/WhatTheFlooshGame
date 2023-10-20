8bee0fad2e01f356d416945376da1d0e4d8b3096
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-10-19 21:56:53 -0300

    Better main.mjs

--- code/main.mjs
+++ code/main.mjs
@@ -8,52 +8,52 @@ import Shop from './scenes/shop.mjs'
 import AFK from './scenes/afk.mjs'
 // initialize context
 kaboom({
-	width: window.innerWidth,
-	height: window.innerHeight,
-	backgroundAudio: true,
-	background: [0,20,102],
-	loadingScreen: false,
-	canvas: document.getElementById("gamecanvas")
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundAudio: true,
+    background: [0, 20, 102],
+    loadingScreen: false,
+    canvas: document.getElementById("gamecanvas")
 })
 
-if(!localStorage.getItem("score")) {
-	localStorage.setItem("score", 0)
+if (!localStorage.getItem("score")) {
+    localStorage.setItem("score", 0)
 }
-if(!localStorage.getItem("coins")) {
-	localStorage.setItem("coins", 0)
+if (!localStorage.getItem("coins")) {
+    localStorage.setItem("coins", 0)
 }
-if(!localStorage.getItem("skin")) {
-	localStorage.setItem("skin", "bean")
+if (!localStorage.getItem("skin")) {
+    localStorage.setItem("skin", "bean")
 }
 
 onLoading((progress) => {
     console.log(progress)
-	// Black background
-	drawRect({
-		width: width(),
-		height: height(),
-		color: rgb(0, 0, 0),
-	})
+    // Black background
+    drawRect({
+        width: width(),
+        height: height(),
+        color: rgb(0, 0, 0),
+    })
 
-	// A pie representing current load progress
-	/*drawCircle({
-		pos: center(),
-		radius: 32,
-		end: map(progress, 0, 1, 0, 360),
-	})
+    // A pie representing current load progress
+    /*drawCircle({
+    	pos: center(),
+    	radius: 32,
+    	end: map(progress, 0, 1, 0, 360),
+    })
 
-	drawText({
-		text: "loading" + ".".repeat(wave(1, 4, time() * 12)),
-		font: "monospace",
-		size: 24,
-		anchor: "center",
-		pos: center().add(0, 70),
-	})*/
+    drawText({
+    	text: "loading" + ".".repeat(wave(1, 4, time() * 12)),
+    	font: "monospace",
+    	size: 24,
+    	anchor: "center",
+    	pos: center().add(0, 70),
+    })*/
 })
 const effects = {
-	vhs: () => ({
-		"u_intensity": 125,
-	})
+    vhs: () => ({
+        "u_intensity": 125,
+    })
 }
 
 let curEffect = 0
@@ -73,21 +73,21 @@ loadShaderURL("vhs", null, "shaders/vhs.frag")
 loadSprite("nerd", "sprites/skins/nerd.png")
 
 const menumusic = play("20190724 2", {
-	loop: true,
-	volume: 0,
+    loop: true,
+    volume: 0,
 })
 const gamemusic = play("20190724", {
-	loop: true,
-	volume: 0,
+    loop: true,
+    volume: 0,
 })
 const creditsmusic = play("20210511", {
-	loop: true,
-	volume: 0
+    loop: true,
+    volume: 0
 })
 const label = add([
-	  pos(8, 8),
-	  text("0"),
-	  area()
+    pos(8, 8),
+    text("0"),
+    area()
 ])
 
 
@@ -96,130 +96,117 @@ const label = add([
 // record(60)
 
 add([
-	uvquad(width(), height()),
-	shader("vhs")
+    uvquad(width(), height()),
+    shader("vhs")
 ])
 
 scene("game", () => {
-	gamemusic.play()
-	menumusic.volume = 0
-	creditsmusic.volume = 0
-	gamemusic.volume = 1
-	Game(effects, curEffect);
+    gamemusic.play()
+    menumusic.volume = 0
+    creditsmusic.volume = 0
+    gamemusic.volume = 1
+    Game(effects, curEffect);
 })
 scene("afk", () => {
-	/*gamemusic.play()
-	menumusic.volume = 0
-	creditsmusic.volume = 0
-	gamemusic.volume = 1*/
-	AFK();
+    /*gamemusic.play()
+    menumusic.volume = 0
+    creditsmusic.volume = 0
+    gamemusic.volume = 1*/
+    AFK();
 })
 burp()
 scene("devOptions", () => {
-	Dev()
+    Dev()
 })
 scene("menu", () => {
-	// burp()
-	// play("score")
-	menumusic.play()
-	menumusic.volume = 1
-	creditsmusic.volume = 0
-	gamemusic.volume = 0
-	Menu();
+    // burp()
+    // play("score")
+    menumusic.play()
+    menumusic.volume = 1
+    creditsmusic.volume = 0
+    gamemusic.volume = 0
+    Menu();
 })
 scene("credits", () => {
-	creditsmusic.play()
-	creditsmusic.volume = 1
-	menumusic.volume = 0
-	gamemusic.volume = 0
-	Credits();
+    creditsmusic.play()
+    creditsmusic.volume = 1
+    menumusic.volume = 0
+    gamemusic.volume = 0
+    Credits();
 })
 scene("shop", () => {
-	creditsmusic.play()
-	creditsmusic.volume = 1
-	menumusic.volume = 0
-	gamemusic.volume = 0
-	Shop();
+    creditsmusic.play()
+    creditsmusic.volume = 1
+    menumusic.volume = 0
+    gamemusic.volume = 0
+    Shop();
 })
 scene("loading", () => {
-	var progress = 0
-	const interval = setInterval((t) => {
-		console.log(progress)
-		progress++;
-		if(progress>99) {
-			console.log("Done!")
-			go("warning")
-			clearInterval(interval)
-			// alert("O jogo está instável no momento, mas ainda é jogável (:")
-			/*alert("AVISO: Tente o máximo NÃO soltar seu dedo, o personagem pode teleportar para exatamente onde você clicar. Isso pode gerar um problema e você pode até mesmo morrer entre as estrelas. Enquanto no computador, tente jogar em tela cheia (F11 + F5)")*/
-		}
-	// Black background
-	drawRect({
-		width: width(),
-		height: height(),
-		color: rgb(0, 0, 0),
-	})
-
-	// A pie representing current load progress
-	drawCircle({
-		pos: center(),
-		radius: 32,
-		end: map(progress, 0, 1, 0, 360),
-	})
-
-	drawText({
-		text: progress + "%\ncarregando" + ".".repeat(wave(1, 4, time() * 12)),
-		font: "monospace",
-		size: 24,
-		anchor: "center",
-		pos: center().add(0, 70),
-	})
-	})
+    var progress = 0
+    const interval = setInterval((t) => {
+        console.log(progress)
+        progress++;
+        if (progress > 99) {
+            console.log("Done!")
+            go("warning")
+            clearInterval(interval)
+            // alert("O jogo está instável no momento, mas ainda é jogável (:")
+            /*alert("AVISO: Tente o máximo NÃO soltar seu dedo, o personagem pode teleportar para exatamente onde você clicar. Isso pode gerar um problema e você pode até mesmo morrer entre as estrelas. Enquanto no computador, tente jogar em tela cheia (F11 + F5)")*/
+        }
+        drawText({
+            text: progress + "%\ncarregando" + ".".repeat(wave(1, 4, time() * 12)),
+            font: "monospace",
+            size: 24,
+            anchor: "center",
+            pos: center().add(0, 70),
+        })
+    })
+    AFK()
 })
 scene("warning", () => {
-	burp()
-	const firstText = add([
-		text("Novidades:", {
-			size: 26
-		}),
-		pos(10,10),
-		area(),
-		scale(2)
-	])
-	const secondText = add([
-		text("- Novo emoji: Nerd\n- O jogo está mais fácil!\n- Correção e melhorias na jogabilidade\n- Atualizado em 19/10/2023\n\n\n- Tente NÃO tirar o seu dedo enquanto estiver jogando, assim o Flush irá mover rapidamente para outro lado, e assim você pode bater em uma estrela e perder o jogo. Enquanto no computador, para evitar isso, tente jogar em tela cheia (F11 + F5).", {
-			size: 18,
-			width: width()
-		}),
-		pos(10, 60),
-		area()
-	])
-	const OK = add([
-		text("Clique em qualquer lugar ou em qualquer tecla para jogar", {
-			size: 14,
-			width: width()
-		}),
-		pos(10, height()-100),
-		// scale(1),
-		area()
-	])
-	onClick(() => go("menu"))
-	onKeyPress(() => go("menu"))
+    burp()
+    const firstText = add([
+        text("Novidades:", {
+            size: 26
+        }),
+        pos(10, 10),
+        area(),
+        scale(2)
+    ])
+    const secondText = add([
+        text("- Novo emoji: Nerd\n- O jogo está mais fácil!\n- Correção e melhorias na jogabilidade\n- Atualizado em 19/10/2023\n\n\n- Tente NÃO tirar o seu dedo enquanto estiver jogando, assim o Flush irá mover rapidamente para outro lado, e assim você pode bater em uma estrela e perder o jogo. Enquanto no computador, para evitar isso, tente jogar em tela cheia (F11 + F5).", {
+            size: 18,
+            width: width()
+        }),
+        pos(10, 60),
+        area()
+    ])
+    const OK = add([
+        text("Clique em qualquer lugar ou em qualquer tecla para jogar", {
+            size: 14,
+            width: width()
+        }),
+        pos(10, height() - 100),
+        // scale(1),
+        area()
+    ])
+    onClick(() => go("menu"))
+    onKeyPress(() => go("menu"))
 })
 scene("notfull", () => {
-	add([
-		pos(center()),
-		sprite("bean"),
-		area(),
-		body()
-	])
-	add([
+    add([
+        pos(center()),
+        sprite("bean"),
+        area(),
+        body()
+    ])
+    add([
         text(`O jogo funciona melhor com tela cheia. Clique em F11 e/ou recarregue a página`, {
             size: 16,
             width: width(),
             // font: "breakout"
         }),
-        pos(0, height()*(3/4)),
+        pos(0, height() * (3 / 4)),
     ]);
 })
 go("loading")
@@ -229,7 +216,7 @@ go("loading")
 	go("notmobile")
 }
 */
-debug.inspect = window.location.hash==="#debug"
+debug.inspect = window.location.hash === "#debug"
 // setFullscreen()
 /*onUpdate(() => {
 	if(!isFullscreen) {
@@ -238,5 +225,5 @@ debug.inspect = window.location.hash==="#debug"
 })*/
 
 onDestroy((e) => {
-	debug.log("Item destruído")
+    debug.log("Item destruído")
 })
