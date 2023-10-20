ae8402f2e357a9a41e095e8136058fc86cf12ec1
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2023-10-19 21:43:09 -0300

    Update afk.mjs

--- code/scenes/afk.mjs
+++ code/scenes/afk.mjs
@@ -1,16 +1,149 @@
 import kaboom from 'kaboom'
 
 export default function AFK() {
-  const player = add([
+  /*const player = add([
 		pos(rand(width()), rand(height())),
 		sprite(localStorage.getItem("skin")),
 		area(),
 		body(),
 		"btn",
 		z(1),
-	])
+	])*/
+   function spawnRect() {
+	    const recta = add([
+            pos(rand(width()), height()),
+		    circle(5, 5),
+            outline(2),
+            area(),
+			offscreen({destroy:true}),
+			"Rect"
+        ])
+		onUpdate(() => {recta.move(0, rand(-150,-100))})
+		wait(rand(0.1, 0.2), spawnRect);
+		// wait(0.3, spawnRect);
+		/*recta.onCollide("clouds", () => {
+			destroy(recta)
+		})*/
+		recta.onUpdate(() => {
+		if (recta.pos.y > height()) {
+			destroy(recta)
+			addKaboom(recta.pos)
+		}
+	})
+	}
+	function spawnCoins() {
+	    const recta = add([
+            pos(rand(width()), height()),
+	    sprite("coin"),
+            scale(0.15),
+            area(),
+			offscreen({destroy:true}),
+			"Coins"
+        ])
+		onUpdate(() => {recta.move(0, rand(-150,-100))})
+		wait(rand(0.5, 1.5), spawnCoins);
+		// wait(0.3, spawnRect);
+		/*recta.onCollide("clouds", () => {
+			destroy(recta)
+		})*/
+		recta.onUpdate(() => {
+		if (recta.pos.y > height()) {
+			destroy(recta)
+			addKaboom(recta.pos)
+		}
+	})
+	}
+	function spawnRedRect() {
+	    const recta = add([
+            pos(rand(width()), height()),
+			/*color(255,0,0),
+		    circle(10, 10),
+            outline(2),*/
+			sprite("star"),
+            area(),
+			offscreen({destroy:true}),
+			"Rectred",
+			// z(rand(0.5, 1)),
+			scale(0.4),
+			rotate(rand(0,360)),
+		        scale(rand(0.2, 0.5))
+        ])
+		onUpdate(() => {recta.move(0, -150)})
+		wait(rand(0.9, 2), spawnRedRect);
+		// wait(0.3, spawnRect);
+		/*recta.onCollide("clouds", () => {
+			destroy(recta)
+		})*/
+		recta.onUpdate(() => {
+		if (recta.pos.y > height()) {
+			destroy(recta)
+			addKaboom(recta.pos)
+		}
+	})
+	}
+	function spawnPlanes() {
+		const recta = add([
+            pos(-100, rand(height())),
+			color(255,0,0),
+		    sprite("plane", {
+				flipX: true
+			}),
+			scale(0.5),
+            area(),
+			offscreen({destroy:true}),
+			"Rectred"
+        ])
+		onUpdate(() => {recta.move(rand(200, 50), -120)})
+		wait(rand(2, 7), spawnPlanes);
+		// wait(0.3, spawnRect);
+		/*recta.onCollide("Rectred", (a) => {
+			destroy(recta)
+			addKaboom(a.pos)
+		})*/
+		recta.onUpdate(() => {
+		if (recta.pos.y > height()) {
+			destroy(recta)
+			addKaboom(recta.pos)
+		}
+	})
+	}
+    function spawnClouds() {
+	    const recta = add([
+            pos(rand(width()), height()),
+		    sprite("cloud"),
+            //outline(2),
+			scale(0.3),
+            area(),
+			offscreen({destroy:true}),
+			"clouds",
+			z(rand(0,2))
+        ])
+		onUpdate(() => {recta.move(rand(-100, -50), -150)})
+		//wait(rand(0.3, 0.7), spawnRect);
+		wait(0.6, spawnClouds);
+		recta.onUpdate(() => {
+		if (recta.pos.y > height()) {
+			destroy(recta)
+			addKaboom(recta.pos)
+		}
+	})
+	}
+	spawnClouds()
+	spawnRedRect()
+	spawnRect()
+	wait(1, spawnCoins)
+	wait(1, spawnPlanes)
 
-  onUpdate(() => {
-    player.move(rand(-1, 1), 0)
-  })
+	onTouchMove(() => {
+		go("menu")
+	})
+	onMouseMove(() => {
+		go("menu")
+	})
+	onKeyDown(() => {
+		go("menu")
+	})
+	onClick(() => {
+		go("menu")
+	})
 }
