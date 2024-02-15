72a391a68033ea77eccd072cb1ad47db22a48293
Igor <80823011+igorwastaken@users.noreply.github.com>
2024-02-15 12:17:59 -0300

    Update game.mjs

--- code/scenes/game.mjs
+++ code/scenes/game.mjs
@@ -49,7 +49,7 @@ export default function Game(velocity=1, spawn=1, coinsSpawn=1) {
     ])
 	player.onUpdate(() => {
 		// player.rotate(rand(100, 200))
-		if(player.pos.y < 300) {
+		if(player.pos.y < width() / 100) {
 			player.move(0,200)
 		}
 	})
